0d3c95d88b2f27edc2c969abb4caf9fd82aaf5ca
Jan <jan.andrade@capgemini.com>
2020-01-10 16:44:50 -0300

    Updates

---
+++ precache-manifest.ad9a604880d719571c4349aba24f4908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387565dff283e3b600a7200484f30303",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "3683ded8d5f75aca184a",
+    "url": "/React-Portfolio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "2f70fb1c93a2c53c6525",
+    "url": "/React-Portfolio/static/css/main.83d2d859.chunk.css"
+  },
+  {
+    "revision": "3683ded8d5f75aca184a",
+    "url": "/React-Portfolio/static/js/2.7e414e60.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Portfolio/static/js/2.7e414e60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f70fb1c93a2c53c6525",
+    "url": "/React-Portfolio/static/js/main.e04932ce.chunk.js"
+  },
+  {
+    "revision": "4583a183fad6bf2ec1b2",
+    "url": "/React-Portfolio/static/js/runtime-main.3d4ad0a1.js"
+  }
+]);
